9ccb8a15252a778236d455ea34795a056ef685a3
Alhassan Osman <oalhassan847@gmail.com>
2023-01-09 04:31:27 +0000

    Implement basic UI with plain HTML

--- main.js
+++ main.js
@@ -20,6 +20,24 @@ const Books = [
       author: ''
     }
   ];
+
+  const bookList = document.querySelector('.book-list');
+
+Books.forEach((Books) => {
+    const book = `
+    <h5>
+    ${Books.title}
+    </h5>
+    <p>
+    ${Books.author}
+    <h5>
+    <button onclick="removeBook(${Books.title}, ${Books.author})">Remove</button>
+    <hr>
+    `;
+    bookList.innerHTML += book;
+  });
+
+  document.bookList.insertBefore(bookList, document.bookList.children[0]);
   
   function addBook(title, author) {
     Books.unshift({title: title, author: author })
